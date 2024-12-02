6a3f18d76d3bc2c3f1e07eda6f28d60a09e45744
Corentin Ludwig <corentin.ludwig@outlook.fr>
2024-12-02 16:22:50 +0100

    day 2 part 2 finish

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
+
 
 fn problem_dampler(line: &Vec<i32>, bad_level: usize) -> bool {
     let mut line_dampler = line.clone();
@@ -9,36 +10,43 @@ fn problem_dampler(line: &Vec<i32>, bad_level: usize) -> bool {
 
     let is_safe = is_safe_level(&line_dampler, false);
 
-    return if is_safe==1 {true} else {false};
+    return if is_safe == 1 { true } else { false };
 }
 
 fn is_safe_level(numbers: &Vec<i32>, use_problem_dampler: bool) -> i32 {
-
-
-
     let size = numbers.len();
     let mut is_safe = true;
-    let is_increasing = if numbers[0]<numbers[1] { true } else { false };
+    let is_increasing = if numbers[0] < numbers[1] { true } else { false };
 
-    if numbers[0]==numbers[1] {
-        is_safe = problem_dampler(numbers, 0);
-        return if is_safe { 1 } else {0};
+
+    if numbers[0] == numbers[1] {
+        if use_problem_dampler {
+            is_safe = problem_dampler(numbers, 0);
+            return if is_safe { 1 } else { 0 };
+        } else {
+            return 0;
+        }
+        
     }
 
-    for i in 0..(size-1) {
-        let test = numbers[i] - numbers[i+1];
+    for i in 0..(size - 1) {
+        let test = numbers[i] - numbers[i + 1];
 
         if is_increasing && test > 0 || !is_increasing && test < 0 {
             if use_problem_dampler {
-                is_safe = problem_dampler(numbers, i);
-                if !is_safe {
-                    is_safe = problem_dampler(numbers, i+1);
+                // Try different indices: i, i+1, 0, and 1
+                let indices_to_try = vec![i, i + 1, 0, 1];
+                for &index in &indices_to_try {
+                    is_safe = problem_dampler(numbers, index);
+                    if is_safe {
+                        break; // Break out of the loop as soon as we find a safe state
+                    }
                 }
             } else {
                 is_safe = false;
             }
             if !is_safe {
-                break;
+                break; // Exit the loop if the sequence is unsafe
             }
         }
 
@@ -46,7 +54,7 @@ fn is_safe_level(numbers: &Vec<i32>, use_problem_dampler: bool) -> i32 {
             if use_problem_dampler {
                 is_safe = problem_dampler(numbers, i);
                 if !is_safe {
-                    is_safe = problem_dampler(numbers, i+1);
+                    is_safe = problem_dampler(numbers, i + 1);
                 }
             } else {
                 is_safe = false;
@@ -57,31 +65,35 @@ fn is_safe_level(numbers: &Vec<i32>, use_problem_dampler: bool) -> i32 {
         }
     }
 
-    return if is_safe { 1 } else {0};
+    return if is_safe { 1 } else { 0 };
 }
 
-pub fn day2(path_input: &str) -> i32 {
 
+pub fn day2(path_input: &str) -> i32 {
     let file = match File::open(path_input) {
         // The `description` method of `io::Error` returns a string that describes the error
-        Err(why) => panic!("couldn't open inputDay2.txt: {}", <dyn Error>::to_string(&why)),
+        Err(why) => panic!(
+            "couldn't open inputDay2.txt: {}",
+            <dyn Error>::to_string(&why)
+        ),
         Ok(file) => file,
     };
 
     let reader = BufReader::new(file);
-    let lines = reader.lines(); 
+    let lines = reader.lines();
     let mut safe_level = 0;
 
     for l in lines {
         match l {
             Ok(content) => {
                 let numbers: Vec<i32> = content
-                .split_whitespace()        // Split the string by whitespace
-                .filter_map(|s| s.parse::<i32>().ok()) // Parse each part into an i32, ignoring errors
-                .collect();
-                safe_level += is_safe_level(&numbers, true)
+                    .split_whitespace() // Split the string by whitespace
+                    .filter_map(|s| s.parse::<i32>().ok()) // Parse each part into an i32, ignoring errors
+                    .collect();
+                    safe_level += is_safe_level(&numbers, true);
 
-            }, // Successfully read a line
+                
+            } // Successfully read a line
             Err(why) => eprintln!("Error reading a line: {}", why), // Handle errors in reading lines
         }
     }

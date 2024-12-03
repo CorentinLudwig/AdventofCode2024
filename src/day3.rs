60165ebc35eae9ebac9c5fa1a1e7216fe0d8a4b9
Corentin Ludwig <corentin.ludwig@outlook.fr>
2024-12-03 13:21:25 +0100

    day3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -17,11 +17,12 @@ fn read_file(path_input: &str) -> usize {
     let reader = BufReader::new(file);
     let lines = reader.lines();
     let mut sum: usize = 0;
+    let mut is_do: bool = true;
 
     for l in lines {
         match l {
             Ok(content) => {
-                sum += find_mul(&content);
+                sum += find_mul(&content, &mut is_do);
             } // Successfully read a line
             Err(why) => eprintln!("Error reading a line: {}", why), // Handle errors in reading lines
         }
@@ -29,19 +30,18 @@ fn read_file(path_input: &str) -> usize {
     return sum;
 }
 
-fn find_mul(l: &str) -> usize {
+fn find_mul(l: &str, is_do: &mut bool) -> usize {
     let mut sum: usize = 0;
-    let mut is_do: bool = true;
     let pattern_mul = Regex::new(r"(mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do\(\))|(don't\(\)))").unwrap();
 
     for caps in pattern_mul.captures_iter(l) {
         // check if it match a do()
         if caps.get(4).is_some() {
-            is_do = true;
+            *is_do = true;
             // check if it match a don't()
         } else if caps.get(5).is_some() {
-            is_do = false;
-        } else if is_do {
+            *is_do = false;
+        } else if *is_do {
             // check if it match a mul
             if caps.get(1).is_some() {
                 let number1: usize = caps[2].parse().unwrap();
